84b0b5f96a4048b4b2ca42aabf60c9c8e5294b1a
sreedhar <sreedharks2003@gmail.com>
2024-02-21 12:37:32 +0530

    feat : arrow icon in search button

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import Sidebar from "@/components/sidebar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import Image from "next/image";
-
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 export default function Home() {
   return (
     <div className="h-screen w-screen border-black border-8">
         <div className="justify-items-center content-center ml-[300px]">
             <div className="flex flex-row space-x-2 m-10">
               <Input type="text" placeholder="Browse for me... !"/>
-              <Button>search</Button>
+              <Button className="gap-1">
+                search
+                <ArrowRightIcon/>                
+              </Button>
             </div>
         </div>
     </div>
